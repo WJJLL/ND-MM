5cd2752eaa3d273580451f6450c93f05db1613a9
WJJLL <47670225+WJJLL@users.noreply.github.com>
2022-08-09 14:10:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,3 +13,23 @@ CUDA_VISIBLE_DEVICES=0 python3 trainND-MM.py  --batch_size 20 --epochs 5 --match
 #ND-MM
 CUDA_VISIBLE_DEVICES=0 python3 trainND-MM.py  --batch_size 20 --epochs 5 --match_target -1  --eps 10  --lam 1 --save_dir result/MM-ND
 CUDA_VISIBLE_DEVICES=0 python3 trainND-MM.py  --batch_size 20 --epochs 5 --match_target 150  --eps 10 --lam 1 --save_dir result/MM-ND
+
+#eval non-target attack
+TARGET_NETS="vgg11 vgg13 vgg16 resnet18 resnet50 resnet101 densenet121 densenet161 vgg19_bn wide_resnet50_2 googlenet"
+for target_net in $TARGET_NETS; do
+  CUDA_VISIBLE_DEVICES=0 python3 eval_non.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens
+  CUDA_VISIBLE_DEVICES=0 python3 eval_non.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens-KL
+  CUDA_VISIBLE_DEVICES=0 python3 eval_non.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens-ND
+  CUDA_VISIBLE_DEVICES=0 python3 eval_non.py  --target_model $target_net   --batch_size 30 --save_dir result/MM
+  CUDA_VISIBLE_DEVICES=0 python3 eval_non.py  --target_model $target_net   --batch_size 30 --save_dir result/MM-ND
+done
+
+#eval target attack
+TARGET_NETS="vgg11 vgg13 vgg16 resnet18 resnet50 resnet101 densenet121 densenet161 vgg19_bn wide_resnet50_2 googlenet"
+for target_net in $TARGET_NETS; do
+  CUDA_VISIBLE_DEVICES=0 python3 eval_Target.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens
+  CUDA_VISIBLE_DEVICES=0 python3 eval_Target.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens-KL
+  CUDA_VISIBLE_DEVICES=0 python3 eval_Target.py  --target_model $target_net   --batch_size 30 --save_dir result/Ens-ND
+  CUDA_VISIBLE_DEVICES=0 python3 eval_Target.py  --target_model $target_net   --batch_size 30 --save_dir result/MM
+  CUDA_VISIBLE_DEVICES=0 python3 eval_Target.py  --target_model $target_net   --batch_size 30 --save_dir result/MM-ND
+done
